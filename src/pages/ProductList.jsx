004c50d33060fff553fe17138d0b9e2b1f706552
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-10-10 15:44:18 -0400

    started the product list

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from "styled-components";
+import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
+
+const Container = styled.div``;
+const Title = styled.h1``;
+const FilterContainer = styled.div``;
+const Filter = styled.div``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar/>
+      <Announcement />
+      <Title>Art</Title>
+      <FilterContainer>
+        <Filter>filter</Filter>
+        <Filter>filter2</Filter>
+      </FilterContainer>
+    </Container>
+  )
+}
+
+export default ProductList
