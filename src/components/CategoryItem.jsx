3d635f4b47ed2952bc572897e97528103bb49f09
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-10-10 11:07:05 -0400

    category made

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -5,20 +5,40 @@ const Container = styled.div`
   flex: 1;
   margin: 3px;
   height: 70vh;
+  position: relative;
 `;
+
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
 `;
+
 const Info = styled.div`
-
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
+
 const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
 
 `;
-const Button = styled.button`
 
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: grey;
+  cursor: pointer;
+  font-weight: 600;
 `;
 
 const CategoryItem = ({item}) => {
