79229e46224cbf3a7dc2a7c7426c9e33898fc2c6
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-10-10 11:01:45 -0400

    started e-store
    stared and nav bar finished and working

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/p47dtFv/bmw.png",
+    title: "BMW",
+    desc: "lEAN MEAN German Machine",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/7zJF9Xf/401413.png",
+    title: "AUDI",
+    desc: "4 rings for the kings of the street",
+    bg: "f5fafd",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/Nxq2ymH/1088.jpg",
+    title: "UNSC",
+    desc: "remember Reach...",
+    bg: "f5fafd",
+  },
+  
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/Gtg9rZ5/scale.jpg",
+    title: "Angel Eyes",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/999nSY3/356775.jpg",
+    title: "QUATTRO",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/qgfzg0Q/halo-awesomeness-v-2-0.jpg",
+    title: "master chief",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id:2,
+    img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id:3,
+    img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id:4,
+    img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id:5,
+    img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id:6,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id:7,
+    img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+  },
+  {
+    id:8,
+    img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+]
